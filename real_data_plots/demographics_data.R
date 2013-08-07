7617743041820cd741e374b04504f616217366a3
Justin Bleich <jbleich89@gmail.com>
2013-08-07 15:09:41 -0400

    initial script for demographics data in case anyone wants to work on it while I'm gone

--- real_data_plots/demographics_data.R
+++ real_data_plots/demographics_data.R
@@ -5,19 +5,19 @@ demo = income
 head(income)
 head(demo$INCOME)
 dim(demo)
-levels(demo$INCOME) = (1:9)*2
+levels(demo$INCOME) = (-4:4)*2
 demo$INCOME = as.numeric(as.character(demo$INCOME))
 levels(demo$AGE) = 1 : 7
 demo$AGE = as.numeric(as.character(demo$AGE))
 levels(demo$EDUCATION) = 1 : 6
-demo$AGE = as.numeric(as.character(demo$AGE))
+demo$EDUCATION = as.numeric(as.character(demo$EDUCATION))
 levels(demo$AREA) = 1 : 5
 demo$AREA = as.numeric(as.character(demo$AREA))
 levels(demo$HOUSEHOLD.SIZE) = 1 : 9
 demo$HOUSEHOLD.SIZE = as.numeric(as.character(demo$HOUSEHOLD.SIZE))
 levels(demo$UNDER18) = 1 : 10
 demo$UNDER18 = as.numeric(as.character(demo$UNDER18))
-str(demo)
+
 
 demo2 = na.omit(demo)
 dim(demo2)
@@ -36,17 +36,19 @@ levels()
 levels(demo$AGE)
 
 
-library(randomForest)
+library(randomForest)W
 rf = randomForest(x = demo2[,-1], y = demo2[,1])
-rf_amdp = amdp(rf, demo2[,-1], "AGE", frac_to_build = 1)
+rf_amdp = amdp(object = rf, X = demo2[,-1], y = demo2[,1], predictor = "AGE")
 partialPlot(rf,pred.data=demo2[,-1],x.var="AGE")
-plot.amdp(rf_amdp,frac_to_plot=.01,plot_pdp=T,rug=T )
+plot.amdp(rf_amdp,frac_to_plot=.01,plot_pdp=T,rug=T, xaxt = "s")
 plot.amdp(rf_amdp,frac_to_plot=.01,plot_pdp=T,rug=T, centered = T )
-cluster_amdp(amdp_obj=rf_amdp,nClusters=2)
+cluster_amdp(amdp_obj=rf_amdp,nClusters=2, plot_legend=T)
 
 
-
-rf_amdp2 = amdp(rf, demo2[,-1], "EDUCATION", frac_to_build = .5)
+rf_amdp2 = amdp(object = rf, X = demo2[,-1], y = demo2[,1], predictor = "EDUCATION")
 partialPlot(rf,pred.data=demo2[,-1],x.var="EDUCATION")
-x = plot.amdp(rf_amdp2,frac_to_plot=.02,plot_pdp=T,rug=T)
-cluster_amdp(amdp_obj=rf_amdp2,nClusters=2)
+par(mfrow = c(1,2))
+plot.amdp(rf_amdp2,frac_to_plot=.002,plot_pdp=T,rug=T)
+cluster_amdp(amdp_obj=rf_amdp2,nClusters=2, plot_legend = T)
+
+table(demo2$EDUCATION)
