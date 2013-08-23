be6cd02c94c770339c460cc21c23cfeeca849f32
Alex Lauf Goldstein <alex@convexity.(none)>
2013-08-23 13:08:29 -0400

    Fixed to use the data() call.

--- real_data_plots/baseball_analysis.R
+++ real_data_plots/baseball_analysis.R
@@ -6,9 +6,9 @@ library(gam)   #gam
 library(amdp)
 library(scatterplot3d);
 
-mlb = read.csv("/home/alex/Data/BakeoffDatasets/baseballsalary.csv",h=T)
-X = mlb[, -1]
-rf_mod = randomForest(salary.thou~.,mlb)
+data(Baseball)
+X = Baseball[, -1]
+rf_mod = randomForest(salary.thou~., Baseball)
 
 BA  = amdp(rf_mod,predictor="BA", X=X)
 RBI = amdp(rf_mod,predictor="RBI", X=X)
