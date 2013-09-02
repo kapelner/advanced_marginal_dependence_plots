299475cd85bd0b25901e8d35423271020a2f6bca
Adam Kapelner <kapelner@gmail.com>
2013-09-02 16:21:18 -0400

    y should not be coerced to numeric when running a classification random forest

--- real_data_plots/pima_analysis.R
+++ real_data_plots/pima_analysis.R
@@ -7,22 +7,22 @@ pima = (Pima.te)
 
 head(pima)
 X = pima[,-8]
-y = as.numeric(pima[,8]) - 1
+y = pima[, 8]
 
 #rf object
 pima_rf = randomForest(x = X, y = y)
-
+names(pima_rf)
 
 amdps = list()
 dampds = list()
 par(mfrow = c(2, 1))
 
 for (j in colnames(X)){
-  amdps[[j]] = amdp(pima_rf, X = X, predictor = j, y = y, logodds = T)
-#  plot(amdps[[j]], frac_to_plot = 0.5, x_quantile = T, centered = T, prop_range_y = F)
-  dampds[[j]] = damdp(amdps[[j]])
-#  plot(dampds[[j]], frac_to_plot = 0.5, plot_sd = T, x_quantile = T, rug = F, prop_range_y = F)
-#  readline("Click for next plot")
+	amdps[[j]] = amdp(pima_rf, X = X, predictor = j, y = y, logodds = T)
+	plot(amdps[[j]], frac_to_plot = 0.5, x_quantile = T, centered = T, prop_range_y = F)
+	dampds[[j]] = damdp(amdps[[j]])
+	plot(dampds[[j]], frac_to_plot = 0.5, plot_sd = T, x_quantile = T, rug = F, prop_range_y = F)
+	readline("Click for next plot")
 }
 
 
