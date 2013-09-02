02e2b7811ea6e401e2450567272530fc00c7c32e
Emil Pitkin <emil.pitkin@gmail.com>
2013-09-02 13:33:58 -0400

    Added damdp plot for gbm interactions for figure 4

--- simulations_for_paper/interaction_model_sims.R
+++ simulations_for_paper/interaction_model_sims.R
@@ -1,3 +1,4 @@
+set.seed(1989)
 ###### script for example 1 in the paper
 library(randomForest)
 library(gbm)
@@ -47,27 +48,34 @@ for(i in 1:100){
 	cat(i); cat(" ")
 }
 
+amdp_obj_rf = amdp(rf_mod, X, predictor = 3, frac_to_build = 0.5)
+
+#plot only 10% of curves with quantiles, actual pdp, and original points. 
+plot(amdp_obj_rf, x_quantile = F, plot_pdp = T, frac_to_plot = 0.5)
+cluster_amdp(amdp_obj_rf, nClusters = 2)
+
+##gbm
+
 gbm_mod = gbm(y ~ ., data = Xy, n.tree = 500, interaction.depth = 3, shrinkage = 0.1, cv.folds = 5)
 ntree = gbm.perf(gbm_mod, method = "cv")
 summary(gbm_mod)
 #lm_mod = lm(y ~ . * . * ., Xy)
 #summary(lm_mod)
 
-amdp_obj_rf = amdp(rf_mod, X, predictor = 3, frac_to_build = 0.5)
 
-#plot only 10% of curves with quantiles, actual pdp, and original points. 
-plot(amdp_obj_rf, x_quantile = F, plot_pdp = T, frac_to_plot = 0.5)
-cluster.amdp(amdp_obj_rf, nClusters = 2)
+amdp_obj_gbm = amdp(gbm_mod, X, y = y, predictor = 3, predictfcn = function(object, newdata){predict(object, newdata, n.tree = ntree)}, frac_to_build = 1)
+damdp_obj_gbm = damdp(amdp_obj_gbm)
 
-
-set.seed(1989)
-amdp_obj_gbm = amdp(gbm_mod, X, predictor = 3, predictfcn = function(object, newdata){predict(object, newdata, n.tree = ntree)}, frac_to_build = 1)
-set.seed(1989)
-plot(amdp_obj_gbm, plot_pdp = T, frac_to_plot = 0.01)
 windows()
-cluster.amdp(amdp_obj_gbm, nClusters = 2, colorvec = c(rgb(0.4, 0.4, 0.4), rgb(0.8, 0.8, 0.8)))
+plot(amdp_obj_gbm, plot_pdp = T, frac_to_plot = 0.01)
+cluster_amdp(amdp_obj_gbm, nClusters = 2)
 
-set.seed(1989)
+windows()
+plot.damdp(damdp_obj_gbm, plot_sd = T, frac_to_plot = 0.05, x_quantile=T, rug = F)
+
+
+#cluster_amdp(amdp_obj_gbm, nClusters = 2, colorvec = c(rgb(0.4, 0.4, 0.4), rgb(0.8, 0.8, 0.8)))
+#
 amdp_obj_lm = amdp(lm_mod, X, predictor = 3, frac_to_build = 0.5)
 
 #plot only 10% of curves with quantiles, actual pdp, and original points. 
