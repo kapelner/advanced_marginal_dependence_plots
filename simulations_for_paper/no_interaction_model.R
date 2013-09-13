e34126d558fc44fce6e114817ff1e74e3e710dcf
Alex Lauf Goldstein <alex@convexity.(none)>
2013-09-13 14:47:09 -0400

    fixed GAM in no interactions model.

--- simulations_for_paper/no_interaction_model.R
+++ simulations_for_paper/no_interaction_model.R
@@ -28,18 +28,23 @@ X  = ex_data$X
 y  = ex_data$y
 
 #build all models
-gam_mod1 = gam(y~s(x_1)+s(x_2),data=Xy)  #no interactions by definition.
-gam_mod2 = gam(y~s(x_1)+s(x_2)+s(x_1*x_2),data=Xy)   #interactions
+gam_mod1 = gbm(y~s(x_1)+s(x_2),data=Xy)  #no interactions by definition.
+gam_mod2 = gbm(y~s(x_1)+s(x_2)+s(x_1*x_2),data=Xy)   #interactions
 rf_mod  = randomForest(y~.,data=Xy)
 
 x1_gam1_amdp = amdp(gam_mod1, X, predictor = 1, frac_to_build = 1)
+x1_gam1_dampd = damdp(x1_gam1_amdp)
 x1_gam2_amdp = amdp(gam_mod2, X, predictor = 1, frac_to_build = 1)
 x1_rf_amdp = amdp(rf_mod, X, predictor = 1, frac_to_build = 1)
 
 #plot only 10% of curves with quantiles, actual pdp, and original points. 
 plot(x1_gam1_amdp$gridpts,apply(x1_gam_amdp$apdps,2,mean),xlab="x_1",ylab="yhat",type='l',lwd=3, ylim=range(x1_gam_amdp$apdps)*1.05)
+
 plot(x1_gam1_amdp, x_quantile = F, plot_pdp = F, frac_to_plot = 0.05)
 
 #caution: alex's machine
 dev.copy2pdf(file="/home/alex/Dropbox/amdp_project/paper/no_inter_example.pdf")
 
+plot(x1_gam1_damdp, x_quantile = F, plot_dpdp = F, frac_to_plot = 0.3,plot_sd=FALSE,
+      plot_orig_pts_deriv=FALSE)
+dev.copy2pdf(file="/home/alex/Dropbox/amdp_project/paper/no_inter_example_dpad.pdf")
